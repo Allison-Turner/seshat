eb2869daba5755b777f475e429a4d0505a86b904
Allison-Turner <hermione2259@gmail.com>
2021-10-11 19:47:49 -0700

    can't have two accesses to a log file at the same time

--- download.py
+++ download.py
@@ -12,17 +12,17 @@ def bash_wget__download(timestamp, loc, ipv, year, month, day, url, topo_choice,
         # Download all files to listed folder location
         for file in properties.itdk_file_types:
             download_log.write("Downloading " + topo_choice + file + ext + "\n")
-            download_cmd = subprocess.Popen(["/usr/bin/wget", "-a", download_log, "-S", "-P", loc, url + year + "-" + month + "/" + topo_choice + file + ext ])
+            download_cmd = subprocess.Popen(["/usr/bin/wget", "-S", "-P", loc, url + year + "-" + month + "/" + topo_choice + file + ext ])
             download_cmd.communicate()
             log_util.log_cmd_results(download_cmd, download_log)
 
         download_log.write("Downloading itdk-run-" + year + month + day + "-dns-names.txt" + ext + "\n")
-        download_cmd = subprocess.Popen(["/usr/bin/wget", "-a", download_log, "-S", "-P", loc, url + year + "-" + month + "/" + "itdk-run-" + year + month + day + "-dns-names.txt" + ext ])
+        download_cmd = subprocess.Popen(["/usr/bin/wget", "-S", "-P", loc, url + year + "-" + month + "/" + "itdk-run-" + year + month + day + "-dns-names.txt" + ext ])
         download_cmd.communicate()
         log_util.log_cmd_results(download_cmd, download_log)
 
         download_log.write("Downloading itdk-run-" + year + month + day + ".addrs" + ext + "\n")
-        download_cmd = subprocess.Popen(["/usr/bin/wget", "-a", download_log, "-S", "-P", loc, url + year + "-" + month + "/" + "itdk-run-" + year + month + day + ".addrs" + ext ])
+        download_cmd = subprocess.Popen(["/usr/bin/wget", "-S", "-P", loc, url + year + "-" + month + "/" + "itdk-run-" + year + month + day + ".addrs" + ext ])
         download_cmd.communicate()
         log_util.log_cmd_results(download_cmd, download_log)
 
@@ -31,7 +31,7 @@ def bash_wget__download(timestamp, loc, ipv, year, month, day, url, topo_choice,
         for file in properties.itdk_file_types:
             if file != ".ifaces":
                 download_log.write("Downloading " + topo_choice + file + ext + "\n")
-                download_cmd = subprocess.Popen(["/usr/bin/wget", "-a", download_log, "-S", "-P", loc, url + year + "-" + month + "/" + topo_choice + file + ext ])
+                download_cmd = subprocess.Popen(["/usr/bin/wget", "-S", "-P", loc, url + year + "-" + month + "/" + topo_choice + file + ext ])
                 download_cmd.communicate()
                 log_util.log_cmd_results(download_cmd, download_log)
 
