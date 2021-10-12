7e5eab0a0efd442d14237f29d8105bfb7e117960
Allison-Turner <hermione2259@gmail.com>
2021-10-11 18:21:15 -0700

    simplifying

--- initialize_db.py
+++ initialize_db.py
@@ -19,13 +19,41 @@ def sqlite__connect(loc, day, month, year, ipv, driver, server, name, user, pwd)
     #cnxn.setencoding(encoding='utf-8')
     cnxn = sqlite3.connect(db_file)
     cursor = cnxn.cursor()
-    cursor.execute("ATTACH DATABASE \'" + db_file + "\' AS " + db_name + ";")
+    # cursor.execute("ATTACH DATABASE \'" + db_file + "\' AS " + db_name + ";")
 
     return cnxn
 
 
 
 def sqlite__create_schema(cursor, user, day, month, year, ipv):
+
+    # Create schemas and tables
+    cursor.execute("CREATE TABLE map_address_to_node(" +
+    """(
+    address inet,
+    node_id integer
+    );
+    """);
+    cursor.execute("CREATE TABLE map_link_to_nodes(" +
+    """
+    link_id integer,
+    node_id_1 integer,
+    address_1 inet, -- optional
+    node_id_2 integer,
+    address_2 inet, -- optional
+    relationship text
+    );
+    """);
+
+    cursor.execute("CREATE TABLE map_node_to_asn(" +
+    """
+    node_id integer,
+    as_number integer
+    );
+    """);
+    cursor.commit()
+
+def sqlite__create_schema1(cursor, user, day, month, year, ipv):
 
     # Create schemas and tables
     cursor.execute("CREATE TABLE ITDK_" + day + "_" + month + "_" + year + "_ipv" + str(ipv) + ".map_address_to_node(" +
