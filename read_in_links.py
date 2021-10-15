48876e431a995c184bcb21bf0606196434280e80
Allison-Turner <hermione2259@gmail.com>
2021-10-14 20:23:57 -0700

    10k i guess??

--- read_in_links.py
+++ read_in_links.py
@@ -73,7 +73,7 @@ def sqlite3__read_in_links(cnxn, loc, topo_choice, ipv):
 
         # limit reads to 100 for development purposes
         found +=1
-        if found > 1000:
+        if found > 10000:
             break
 
     links_file.close()
